638e3eae498c386b8227cf3b645c82affb32d82a
mikemertens15 <mikemertens15@gmail.com>
2023-10-24 12:29:02 -0400

    Refactored to use MaterialUI

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,30 +1,34 @@
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Link as Material_Link,
+  Avatar,
+} from "@mui/material";
+
 import { UserContext } from "../Context/UserContext";
-import Logo from "./Logo";
+import Copyright from "./Copyright";
 
 const LoginForm: React.FC = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
   const context = useContext(UserContext);
   if (!context) {
     throw new Error("LoginForm must be used within a UserProvider");
   }
   const { setUser } = context;
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const userToSignIn = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
 
     try {
       const response = await fetch("http://localhost:3001/api/login", {
@@ -32,7 +36,7 @@ const LoginForm: React.FC = () => {
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(userToSignIn),
       });
 
       const data = await response.json();
@@ -56,35 +60,67 @@ const LoginForm: React.FC = () => {
   };
 
   return (
-    <div>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <Logo />
-        <input
-          type="email"
-          className="text-field"
-          placeholder="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          sx={{ m: 1, width: 100, height: 100, bgcolor: "secondary.main" }}
+          src="/wealthsphere-logo-maybe.png"
         />
-        <input
-          type="password"
-          className="text-field"
-          placeholder="password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-        <button type="submit" className="submit-button">
-          Log In
-        </button>
-        <div className="register-message">
-          <p>
-            New User? <Link to="/register">Register Here</Link>{" "}
-          </p>
-        </div>
-      </form>
-    </div>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="email"
+            id="email"
+            label="email address"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            id="password"
+            label="password"
+            autoComplete="current-password"
+            type="password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Material_Link href="#" variant="body2">
+                Forgot Password?
+              </Material_Link>
+            </Grid>
+            <Grid item>
+              <Material_Link href="/register" variant="body2">
+                {"Don't have an Account? Sign Up"}
+              </Material_Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 };
 
